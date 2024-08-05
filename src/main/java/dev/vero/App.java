872af0c983403b2ae1994e1996303de64465f9c5
Miharu669 <veronicadoelfuentes@gmail.com>
2024-08-05 09:44:33 +0200

    first commit

---
+++ src/main/java/dev/vero/App.java
@@ -0,0 +1,17 @@
+package dev.vero;
+
+/**
+ * Hello world!
+ */
+public final class App {
+    private App() {
+    }
+
+    /**
+     * Says hello to the world.
+     * @param args The arguments of the program.
+     */
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
